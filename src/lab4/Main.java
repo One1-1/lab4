557fa19ed88d6b333695d63db62f7b4500c775d4
One1-1 <124714941+One1-1@users.noreply.github.com>
2025-06-02 12:24:15 +0300

    Update Main.java

--- src/lab4/Main.java
+++ src/lab4/Main.java
@@ -1,5 +1,6 @@
 package lab4;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -21,16 +22,55 @@ public class Main {
             
             choice = scanner.nextInt();
             switch (choice) {
-                case 1: MyClass.task1_lab2(scanner); break;
-                case 2: MyClass.task2_lab2(scanner); break;
-                case 3: MyClass.task3_lab2(scanner); break;
-                case 4: MyClass.task4_lab2(scanner); break;
-                case 5: MyClass.task1_lab3(scanner); break;
-                case 6: MyClass.task2_lab3(scanner); break;
-                case 7: MyClass.task3_lab3_nonRecursive(scanner); break;
-                case 8: MyClass.task3_lab3_recursive(scanner); break;
-                case 0: System.out.println("Завершение работы"); break;
-                default: System.out.println("Неверный выбор!");
+                case 1:
+                    double[] result1 = MyClass.task1_lab2(scanner);
+                    System.out.println("Среднее геометрическое: " + result1[0]);
+                    System.out.println("Дробная часть: " + result1[1]);
+                    break;
+                case 2:
+                    boolean result2 = MyClass.task2_lab2(scanner);
+                    System.out.println("isSpecial = " + result2);
+                    break;
+                case 3:
+                    double result3 = MyClass.task3_lab2(scanner);
+                    System.out.println("y = " + result3);
+                    break;
+                case 4:
+                    double[] result4 = MyClass.task4_lab2(scanner);
+                    if (result4 != null) {
+                        System.out.printf("Накрутка: %.2f%%\n", result4[0]);
+                        System.out.printf("Итог: %.2f\n", result4[1]);
+                    }
+                    break;
+                case 5:
+                    String result5 = MyClass.task1_lab3(scanner);
+                    System.out.println(result5);
+                    break;
+                case 6:
+                    int result6 = MyClass.task2_lab3(scanner);
+                    System.out.println("Количество: " + result6);
+                    break;
+                case 7:
+                    List<Integer> result7 = MyClass.task3_lab3_nonRecursive(scanner);
+                    System.out.println("Результат (нерекурсивный):");
+                    for (int num : result7) {
+                        System.out.print(num + " ");
+                    }
+                    System.out.println();
+                    break;
+                case 8:
+                    List<Integer> result8 = MyClass.task3_lab3_recursive(scanner);
+                    System.out.println("Результат (рекурсивный):");
+                    for (int num : result8) {
+                        System.out.print(num + " ");
+                    }
+                    System.out.println();
+                    break;
+                case 0:
+                    System.out.println("Завершение работы");
+                    break;
+                default:
+                    System.out.println("Неверный выбор!");
             }
         } while (choice != 0);
         scanner.close();
